883b328a7b79bc7e080be962d5fb281ce641e343
blackgirlbytes <blackgirlbytes@github.com>
2023-08-01 14:18:02 -0400

    update test

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -8,19 +8,27 @@ test('it should have a title', async ({ page }) => {
   await expect(page).toHaveTitle('Web5 To Do App');
 });
 
-test('it should add a task', async ({ page }) => {
+test('it should load the homepage', async ({ page }) => {
   await page.goto('http://localhost:3000');
   const eyes = new Eyes();
   await eyes.open(page, 'To', 'Do');
-
-  // Fill in the task input and click the add button
-  await page.fill('input[placeholder="New task"]', 'Test task');
-  await page.click('button[type="submit"]');
-
-  // Check the page visually
-  await eyes.check('After adding a task', Target.window().fully());
-
+  await eyes.check(Target.window().fully())
   await eyes.close();
 });
+
+// test('it should add a task', async ({ page }) => {
+//   await page.goto('http://localhost:3000');
+//   const eyes = new Eyes();
+//   await eyes.open(page, 'To', 'Do');
+
+//   // Fill in the task input and click the add button
+//   await page.fill('input[placeholder="New task"]', 'Test task');
+//   await page.click('button[type="submit"]');
+
+//   // Check the page visually
+//   await eyes.check('After adding a task', Target.window().fully());
+
+//   await eyes.close();
+// });
 
 
